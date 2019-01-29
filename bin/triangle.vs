789bd44e5bebd639cbc3c698969010f3f18aa338
marika <marika@waifu.club>
2019-01-29 16:07:47 -0400

    Add a perspective triangle demo to main.c.

---
+++ bin/triangle.vs
@@ -0,0 +1,8 @@
+attribute vec3 vertexPosition;
+
+uniform mat4 mvp;
+
+void main()
+{
+    gl_Position = mvp * vec4(vertexPosition, 1);
+}
--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include <GLES2/gl2.h>
 
+#define MATH_3D_IMPLEMENTATION
+#include <arkanis/math_3d.h>
+
 #include "screen.h"
 #include "shader.h"
 #include "program.h"
@@ -14,23 +17,44 @@ int main()
     // create the shaders and program
     GLuint vertex, fragment, program;
 
-    create_shader(&vertex, GL_VERTEX_SHADER, "/home/pi/projects/vvd/bin/test.vs");
-    create_shader(&fragment, GL_FRAGMENT_SHADER, "/home/pi/projects/vvd/bin/test.fs");
+    create_shader(&vertex, GL_VERTEX_SHADER, "/home/pi/projects/vvd/bin/triangle.vs");
+    create_shader(&fragment, GL_FRAGMENT_SHADER, "/home/pi/projects/vvd/bin/triangle.fs");
     create_program(&program, vertex, fragment);
 
-    // the vertex buffer data to display
-    GLfloat vertex_buffer_data[] =
+    // get uniform and attribute locations
+    GLuint mvp_uniform = glGetUniformLocation(program, "mvp");
+    GLuint vertex_position_attribute = glGetAttribLocation(program, "vertexPosition");
+
+    // vertex buffer data for a fullscreen triangle
+    const GLfloat vertex_buffer_data[] =
     {
-        -1.0f, -1.0f, 0.0f,
-         1.0f, -1.0f, 0.0f,
-         0.0f,  1.0f, 0.0f,
+        -1.0f, -1.0f,  0.0f, // bottom left
+         1.0f, -1.0f,  0.0f, // bottom right
+         0.0f,  1.0f,  0.0f, // top center
     };
 
-    // load the vertex buffer data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertex_buffer_data);
+    // setup the vertex buffer
+    GLuint vertex_buffer;
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
 
-    // enable the vertex buffer data
-    glEnableVertexAttribArray(0);
+    // create the projection, view, and model matrices
+    mat4_t projection = m4_perspective(45.0f,
+                                       (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT,
+                                       0.1f,
+                                       100.0f);
+
+    mat4_t view = m4_look_at(vec3(4, 3, 3),
+                             vec3(0, 0, 0),
+                             vec3(0, 1, 0));
+
+    mat4_t model = m4_identity();
+
+    // calculate the mvp matrix
+    mat4_t mvp = projection;
+    mvp = m4_mul(mvp, view);
+    mvp = m4_mul(mvp, model);
 
     while (1)
     {
@@ -40,16 +64,22 @@ int main()
         // clear the colour buffer
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw the vertex buffer data as triangles
+        // use the shader program
+        glUseProgram(program);
+
+        // send the mvp matrix to the shader
+        glUniformMatrix4fv(mvp_uniform, 1, GL_FALSE, &mvp.m[0][0]);
+
+        // pass the vertex buffer data to the vertex shader
+        glEnableVertexAttribArray(vertex_position_attribute);
+        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+        glVertexAttribPointer(vertex_position_attribute, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+        // draw the triangle
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // render and swap the framebuffer
-        void *buffer = malloc(SCREEN_WIDTH * SCREEN_HEIGHT * 4);
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        glReadPixels(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-
-        // free the framebuffer now that its drawn
-        free(buffer);
+        // disable the vertex attribute array
+        glDisableVertexAttribArray(vertex_position_attribute);
 
         // update the screen
         update_screen(screen);
@@ -62,7 +92,12 @@ int main()
             usleep((1000.0 / SCREEN_RATE) - frame_time);
     }
 
-    // free the screen from memory
+    // delete everything
+    glDeleteBuffers(1, &vertex_buffer);
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+    glDeleteProgram(program);
+
     free(screen);
 
     return 0;
