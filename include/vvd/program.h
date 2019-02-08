36fc631800380921ab8530de095d5b364c183aed
marika <marika@waifu.club>
2019-02-08 11:31:23 -0400

    Add PVM matrices support to Program.

--- include/vvd/program.h
+++ include/vvd/program.h
@@ -1,18 +1,38 @@
 #pragma once
 
+#include <stdbool.h>
 #include <GLES2/gl2.h>
+#include <arkanis/math_3d.h>
 
 typedef struct
 {
     GLuint id;
     GLuint vertex_id, fragment_id;
+
+    // whether or not this programs vertex shader takes projection/view/model matrices
+    bool accepts_pvm_matrices;
+
+    // uniforms for the projection, view, and model matrices
+    // only used if accepts_pvm_matrices is true
+    GLuint uniform_projection_id;
+    GLuint uniform_view_id;
+    GLuint uniform_model_id;
 } Program;
 
-// source paths should follow the same patterns of shader_creates source_path
-Program *program_create(const char *vertex_source_path, const char *fragment_source_path);
+// create a shader program from the given vertex and fragment source paths
+// accepts_pvm_matrices should be true if the vertex shader takes projection, view, and model matrices
+Program *program_create(const char *vertex_source_path, const char *fragment_source_path, bool accepts_pvm_matrices);
 void program_free(Program *program);
 
+// get the id of a uniform in the given programs vertex shader
 GLuint program_get_uniform_id(Program *program, const GLchar *name);
+
+// get the id of an attribute in the given programs vertex shader
 GLuint program_get_attribute_id(Program *program, const GLchar *name);
 
+// call glUseProgram with the given program
 void program_use(Program *program);
+
+// set the projection, view, and model matrices of the given program
+// exits if program->accepts_pvm_matrices is false
+void program_set_matrices(Program *program, mat4_t projection, mat4_t view, mat4_t model);
--- src/program.c
+++ src/program.c
@@ -38,11 +38,12 @@ void program_assert_error(GLuint program, GLenum pname)
     assert(check == GL_TRUE);
 }
 
-Program *program_create(const char *vertex_source_path, const char *fragment_source_path)
+Program *program_create(const char *vertex_source_path, const char *fragment_source_path, bool accepts_pvm_matrices)
 {
     // create the program
     Program *program = malloc(sizeof(Program));
     program->id = glCreateProgram();
+    program->accepts_pvm_matrices = accepts_pvm_matrices;
 
     // create the vertex and fragment shaders
     shader_create(&program->vertex_id, GL_VERTEX_SHADER, vertex_source_path);
@@ -58,8 +59,18 @@ Program *program_create(const char *vertex_source_path, const char *fragment_sou
     // make sure there are no link errors
     program_assert_error(program->id, GL_LINK_STATUS);
 
-    // use the program
-    glUseProgram(program->id);
+    // get the matrices uniforms if the vertex shader accepts pvm matrices
+    if (accepts_pvm_matrices)
+    {
+        program->uniform_projection_id = program_get_uniform_id(program, "projection");
+        program->uniform_view_id = program_get_uniform_id(program, "view");
+        program->uniform_model_id = program_get_uniform_id(program, "model");
+
+        // assert that all the uniforms were found
+        assert(program->uniform_projection_id >= 0);
+        assert(program->uniform_view_id >= 0);
+        assert(program->uniform_model_id >= 0);
+    }
 
     // return the program
     return program;
@@ -86,4 +97,15 @@ GLuint program_get_attribute_id(Program *program, const GLchar *name)
 void program_use(Program *program)
 {
     glUseProgram(program->id);
+}
+
+void program_set_matrices(Program *program, mat4_t projection, mat4_t view, mat4_t model)
+{
+    // assert that the vertex shader accepts pvm matrices
+    assert(program->accepts_pvm_matrices);
+
+    // set the matrices
+    glUniformMatrix4fv(program->uniform_projection_id, 1, GL_FALSE, &projection.m[0][0]);
+    glUniformMatrix4fv(program->uniform_view_id, 1, GL_FALSE, &view.m[0][0]);
+    glUniformMatrix4fv(program->uniform_model_id, 1, GL_FALSE, &model.m[0][0]);
 }
