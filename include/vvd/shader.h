1bf9570f5a929770f0cea86979263a1dc5b846b4
marika <marika@waifu.club>
2019-02-11 16:04:56 -0400

    Add comment about shader paths in shader.h.

--- include/vvd/shader.h
+++ include/vvd/shader.h
@@ -3,4 +3,6 @@
 #include <GLES2/gl2.h>
 
 // creates a shader of the given type with source from the given source path and sets shader to its id
+// the source path should be relative to the shaders folder
+// program_create should almost always be used instead of this directly
 void shader_create(GLuint *shader, GLenum type, const char *source_path);
