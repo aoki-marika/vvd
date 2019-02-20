11cf5d280ecba3e1c7b7d405b913d3245692893c
marika <marika@waifu.club>
2019-02-20 11:04:16 -0400

    Change Mesh to take max_vertices instead of num_vertices, and change mesh_draw to take a size and offset in vertices.
    mesh_draw_all has the old functionality of mesh_draw where it draws all the vertices.

--- include/vvd/mesh.h
+++ include/vvd/mesh.h
@@ -6,22 +6,29 @@
 
 #include "program.h"
 
-// number of vertices for basic data types
+// the number of values in one vertex (x, y, z)
+#define MESH_VERTEX_VALUES 3
+
+// the number of vertices for basic shapes
 #define MESH_VERTICES_TRIANGLE 3
 #define MESH_VERTICES_QUAD MESH_VERTICES_TRIANGLE * 2
 
 typedef struct
 {
+    // the id of this meshes vertex buffer
     GLuint vertex_buffer_id;
-    GLuint num_vertices;
 
+    // the maximum number of vertices in this meshes vertex buffer
+    GLuint max_vertices;
+
+    // the program for this meshes geometry and material
     Program *program;
     GLuint attribute_vertex_position_id;
 } Mesh;
 
-// create a new mesh of the given type with the given number of vertices using the given program for its geometry and material
+// create a new mesh of the given type with the given number of maximum vertices using the given program for its geometry and material
 // the given programs vertex shader must have a vec3 vertexPosition attribute
-Mesh *mesh_create(int num_vertices, Program *program, GLenum usage);
+Mesh *mesh_create(int max_vertices, Program *program, GLenum usage);
 void mesh_free(Mesh *mesh);
 
 // set the given meshes vertices to the given vertices, offset by the given vertice index
@@ -34,5 +41,8 @@ void mesh_set_vertices_quad(Mesh *mesh, int index, GLfloat width, GLfloat height
 // a helper method for calling mesh_set_vertices with a quad generated from the given width and height and position
 void mesh_set_vertices_quad_pos(Mesh *mesh, int index, GLfloat width, GLfloat height, vec3_t position);
 
-// draw the given mesh
-void mesh_draw(Mesh *mesh);
+// draw the given meshes vertices starting at the given vertex index and of the given size
+void mesh_draw(Mesh *mesh, int index, size_t size);
+
+// draw all of the given meshes vertices
+void mesh_draw_all(Mesh *mesh);
--- src/mesh.c
+++ src/mesh.c
@@ -2,11 +2,11 @@
 
 #include <assert.h>
 
-Mesh *mesh_create(int num_vertices, Program *program, GLenum usage)
+Mesh *mesh_create(int max_vertices, Program *program, GLenum usage)
 {
     // create the mesh
     Mesh *mesh = malloc(sizeof(Mesh));
-    mesh->num_vertices = num_vertices;
+    mesh->max_vertices = max_vertices;
     mesh->program = program;
 
     // get the vertex position attribute
@@ -18,7 +18,7 @@ Mesh *mesh_create(int num_vertices, Program *program, GLenum usage)
 
     // allocate the vertex buffer data
     glBindBuffer(GL_ARRAY_BUFFER, mesh->vertex_buffer_id);
-    glBufferData(GL_ARRAY_BUFFER, num_vertices * 3 * sizeof(float), NULL, usage);
+    glBufferData(GL_ARRAY_BUFFER, mesh->max_vertices * MESH_VERTEX_VALUES * sizeof(float), NULL, usage);
 
     // return the mesh
     return mesh;
@@ -34,7 +34,7 @@ void mesh_set_vertices(Mesh *mesh, int index, const GLfloat *vertices, size_t ve
 {
     // bind the vertex buffer and set its vertices starting at offset
     glBindBuffer(GL_ARRAY_BUFFER, mesh->vertex_buffer_id);
-    glBufferSubData(GL_ARRAY_BUFFER, index * 3 * sizeof(float), vertices_size, vertices);
+    glBufferSubData(GL_ARRAY_BUFFER, index * MESH_VERTEX_VALUES * sizeof(float), vertices_size, vertices);
 }
 
 void mesh_set_vertices_quad(Mesh *mesh, int index, GLfloat width, GLfloat height)
@@ -77,16 +77,25 @@ void mesh_set_vertices_quad_pos(Mesh *mesh, int index, GLfloat width, GLfloat he
     mesh_set_vertices(mesh, index, vertices, sizeof(vertices));
 }
 
-void mesh_draw(Mesh *mesh)
+void mesh_draw(Mesh *mesh, int index, size_t size)
 {
+    // assert that the index and size are valid
+    assert(index >= 0 && index < mesh->max_vertices);
+    assert(index + size <= mesh->max_vertices);
+
     // pass the vertex buffer vertices to the mesh program
     glEnableVertexAttribArray(mesh->attribute_vertex_position_id);
     glBindBuffer(GL_ARRAY_BUFFER, mesh->vertex_buffer_id);
-    glVertexAttribPointer(mesh->attribute_vertex_position_id, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glVertexAttribPointer(mesh->attribute_vertex_position_id, MESH_VERTEX_VALUES, GL_FLOAT, GL_FALSE, 0, NULL);
 
     // draw the mesh
-    glDrawArrays(GL_TRIANGLES, 0, mesh->num_vertices);
+    glDrawArrays(GL_TRIANGLES, index, size);
 
     // disable the vertex attribute array
     glDisableVertexAttribArray(mesh->attribute_vertex_position_id);
+}
+
+void mesh_draw_all(Mesh *mesh)
+{
+    mesh_draw(mesh, 0, mesh->max_vertices);
 }
