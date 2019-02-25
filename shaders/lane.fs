c0b849a45001eda01d5075801c6a8b62f8eccede
marika <marika@waifu.club>
2019-02-25 16:47:42 -0400

    Comment lane.fs.

--- shaders/lane.fs
+++ shaders/lane.fs
@@ -3,10 +3,13 @@ uniform int lane;
 void main()
 {
     if (lane == 0)
+        // notes
         gl_FragColor = vec4(0.2, 0.2, 0.2, 1);
     else if (lane == 1)
+        // left analog
         gl_FragColor = vec4(0.23, 0.25, 0.32, 1);
     else if (lane == 2)
+        // right analog
         gl_FragColor = vec4(0.3, 0.22, 0.27, 1);
 }
 
