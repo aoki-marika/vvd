dec81ba49ed23eb0237cfa7ab278da1879121dfd
marika <marika@waifu.club>
2019-02-26 12:40:56 -0400

    Add judgement.h for Judgement enum and timing windows.

---
+++ include/vvd/judgement.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "screen.h"
+
+// timing windows for each judgement, in milliseconds
+// these windows are both before and after (+-)
+// todo: unsure if near/error windows are correct
+#define JUDGEMENT_CRITICAL_WINDOW 2 * SCREEN_FRAME_DURATION
+#define JUDGEMENT_NEAR_WINDOW 4 * SCREEN_FRAME_DURATION
+#define JUDGEMENT_ERROR_WINDOW 8 * SCREEN_FRAME_DURATION
+#define JUDGEMENT_ANALOG_SLAM_WINDOW JUDGEMENT_CRITICAL_WINDOW + JUDGEMENT_NEAR_WINDOW
+
+typedef enum
+{
+    JudgementCritical,
+    JudgementNear,
+    JudgementError,
+} Judgement;
