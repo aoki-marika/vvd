72035c18993382d4c396018970c5d3ba46405d0d
marika <marika@waifu.club>
2019-02-27 12:32:47 -0400

    Add hold processing to Scoring.

--- include/vvd/scoring.h
+++ include/vvd/scoring.h
@@ -16,35 +16,17 @@ typedef struct
     Note *current_fx_notes[CHART_FX_LANES];
     int current_fx_note_indexes[CHART_FX_LANES];
 
-    // whether or not each bt/fx note in chart has been judged
-    bool *bt_notes_judged[CHART_BT_LANES];
-    bool *fx_notes_judged[CHART_FX_LANES];
+    // whether or not each bt/fx chip in chart has been judged
+    bool *bt_chips_judged[CHART_BT_LANES];
+    bool *fx_chips_judged[CHART_FX_LANES];
+
+    // whether or not the button of each lane is held on the current hold, if any
+    bool bt_holds_held[CHART_BT_LANES];
+    bool fx_holds_held[CHART_FX_LANES];
 } Scoring;
 
 Scoring *scoring_create(Chart *chart);
 void scoring_free(Scoring *scoring);
-
-// Judgement scoring_[bt/fx]_note_passed(Scoring *scoring, int lane, int index)
-//  - can have a JudgementError in the case of an unjudged note passing its maximum window
-//
-// void scoring_[bt/fx]_note_current(Scoring *scoring, int lane, int index)
-//  - no judgements possible as it just sets state
-//
-// Judgement scoring_[bt/fx]_state_changed(Scoring *scoring, int lane, bool pressed, double time)
-//  - can have JudgementCritical, JudgementNear, or JudgementError for chips
-//  - should trigger beams for the given lane when judgement is crit/near/error
-//
-// scoring_subbeat_changed(Scoring *scoring, uint16_t subbeat, Judgement bt_holds[CHART_BT_LANES], Judgement fx_holds[CHART_FX_LANES])
-//  - can have JudgementCritical or JudgementError for every hold and analog
-//  - JudgementCritical means the note/analog was held and should add 1 to the chain, and set the current notes state to critical
-//  - JudgementError means the note/analog was not held and should reset the chain, and set the current note/analogs state to error
-
-// tell the given scoring that the given bt/fx note in the given lane has passed its maximum hit window after being current
-// should always be called before scoring_[bt/fx]_note_current
-// returns the judgement, if any, for the passed note on the given lane at the given index
-// chips return a JudgementError if it was not judged before passing its maximum timing window
-Judgement scoring_bt_note_passed(Scoring *scoring, int lane, int index);
-Judgement scoring_fx_note_passed(Scoring *scoring, int lane, int index);
 
 // tell the given scoring that the given bt/fx note in the given lane has become current
 void scoring_bt_note_current(Scoring *scoring, int lane, int index);
@@ -56,3 +38,11 @@ void scoring_fx_note_current(Scoring *scoring, int lane, int index);
 // chips return the judgement for the closest matching timing window for the given time if the given state is pressed
 Judgement scoring_bt_state_changed(Scoring *scoring, int lane, bool pressed, double time);
 Judgement scoring_fx_state_changed(Scoring *scoring, int lane, bool pressed, double time);
+
+// tell the given scoring that the current tick changed to the given tick, which occured at the given subbeat
+// sets bt_hold_judgements and fx_hold_judgements to the judgement for the given tick of each hold on each lane, if any, of their respective types
+void scoring_tick_changed(Scoring *scoring,
+                          int tick,
+                          int subbeat,
+                          Judgement bt_hold_judgements[CHART_BT_LANES],
+                          Judgement fx_hold_judgements[CHART_FX_LANES]);
--- src/scoring.c
+++ src/scoring.c
@@ -10,19 +10,20 @@ Scoring *scoring_create(Chart *chart)
     // set the scorings properties
     scoring->chart = chart;
 
-    // default the current bt and fx notes to null
+    // default and allocate all the properties
     for (int i = 0; i < CHART_BT_LANES; i++)
+    {
         scoring->current_bt_notes[i] = NULL;
+        scoring->bt_chips_judged[i] = calloc(chart->num_bt_notes[i], sizeof(bool));
+        scoring->bt_holds_held[i] = false;
+    }
 
     for (int i = 0; i < CHART_FX_LANES; i++)
+    {
         scoring->current_fx_notes[i] = NULL;
-
-    // allocate all the judged arrays
-    for (int i = 0; i < CHART_BT_LANES; i++)
-        scoring->bt_notes_judged[i] = calloc(chart->num_bt_notes[i], sizeof(bool));
-
-    for (int i = 0; i < CHART_FX_LANES; i++)
-        scoring->fx_notes_judged[i] = calloc(chart->num_fx_notes[i], sizeof(bool));
+        scoring->fx_chips_judged[i] = calloc(chart->num_fx_notes[i], sizeof(bool));
+        scoring->fx_holds_held[i] = false;
+    }
 
     // return the scoring
     return scoring;
@@ -32,36 +33,38 @@ void scoring_free(Scoring *scoring)
 {
     // free all the judged arrays
     for (int i = 0; i < CHART_BT_LANES; i++)
-        free(scoring->bt_notes_judged[i]);
+        free(scoring->bt_chips_judged[i]);
 
     for (int i = 0; i < CHART_FX_LANES; i++)
-        free(scoring->fx_notes_judged[i]);
+        free(scoring->fx_chips_judged[i]);
 
     // free the scoring
     free(scoring);
 }
 
 Judgement note_passed(Note **current_notes,
-                     bool **notes_judged,
-                     Note **notes,
-                     int lane,
-                     int index)
+                      bool *holds_held,
+                      bool **chips_judged,
+                      Note **notes,
+                      int lane,
+                      int index)
 {
     // the judgement to return
     Judgement judgement = JudgementNone;
 
     // if the note is a chip and was not judged
-    if (!notes[lane][index].hold && !notes_judged[lane][index])
+    if (!notes[lane][index].hold && !chips_judged[lane][index])
     {
         // set the judgement to error
         judgement = JudgementError;
 
-        // mark the note as now judged
-        notes_judged[lane][index] = true;
+        // mark the chip as now judged
+        chips_judged[lane][index] = true;
     }
 
-    // reset the current note for the given lane
+    // reset the current note and held state for the given lane
     current_notes[lane] = NULL;
+    holds_held[lane] = false;
 
     // return the judgement
     return judgement;
@@ -71,7 +74,8 @@ Judgement scoring_bt_note_passed(Scoring *scoring, int lane, int index)
 {
     // handle the passed note and return the judgement
     return note_passed(scoring->current_bt_notes,
-                       scoring->bt_notes_judged,
+                       scoring->bt_holds_held,
+                       scoring->bt_chips_judged,
                        scoring->chart->bt_notes,
                        lane,
                        index);
@@ -81,7 +85,8 @@ Judgement scoring_fx_note_passed(Scoring *scoring, int lane, int index)
 {
     // handle the passed note and return the judgement
     return note_passed(scoring->current_fx_notes,
-                       scoring->fx_notes_judged,
+                       scoring->fx_holds_held,
+                       scoring->fx_chips_judged,
                        scoring->chart->fx_notes,
                        lane,
                        index);
@@ -124,7 +129,8 @@ Judgement judgement_for_chip(Note *note, double time)
 
 Judgement note_state_changed(Note **current_notes,
                              int *current_note_indexes,
-                             bool **notes_judged,
+                             bool *holds_held,
+                             bool **chips_judged,
                              int lane,
                              bool pressed,
                              double time)
@@ -132,23 +138,24 @@ Judgement note_state_changed(Note **current_notes,
     // if there is a current note on the given lane
     if (current_notes[lane])
     {
-        // return if the current note is already judged
-        if (notes_judged[lane][current_note_indexes[lane]])
-            return JudgementNone;
-
         // get the current note for the given lane
         Note *note = current_notes[lane];
+
+        // return if the current chip is already judged
+        if (!note->hold && chips_judged[lane][current_note_indexes[lane]])
+            return JudgementNone;
 
         // if the current note is a hold
         if (note->hold)
         {
-            // todo: hold processing
+            // set the given lanes hold held state
+            holds_held[lane] = pressed;
         }
         // if the current note is a chip and the button is pressed
         else if (pressed)
         {
-            // mark the note as judged
-            notes_judged[lane][current_note_indexes[lane]] = true;
+            // mark the chip as judged
+            chips_judged[lane][current_note_indexes[lane]] = true;
 
             // return the judgement for the current chip and time
             return judgement_for_chip(note, time);
@@ -164,7 +171,8 @@ Judgement scoring_bt_state_changed(Scoring *scoring, int lane, bool pressed, dou
     // process the given state
     return note_state_changed(scoring->current_bt_notes,
                               scoring->current_bt_note_indexes,
-                              scoring->bt_notes_judged,
+                              scoring->bt_holds_held,
+                              scoring->bt_chips_judged,
                               lane,
                               pressed,
                               time);
@@ -175,8 +183,74 @@ Judgement scoring_fx_state_changed(Scoring *scoring, int lane, bool pressed, dou
     // process the given state
     return note_state_changed(scoring->current_fx_notes,
                               scoring->current_fx_note_indexes,
-                              scoring->fx_notes_judged,
+                              scoring->fx_holds_held,
+                              scoring->fx_chips_judged,
                               lane,
                               pressed,
                               time);
+}
+
+void tick_holds(int num_lanes,
+                Note *current_notes[num_lanes],
+                bool holds_held[num_lanes],
+                int tick,
+                int subbeat,
+                Judgement judgements[num_lanes])
+{
+    for (int i = 0; i < num_lanes; i++)
+    {
+        // reset the current lanes judgement
+        judgements[i] = JudgementNone;
+
+        // if there is a current note on the current lane
+        if (current_notes[i])
+        {
+            Note *note = current_notes[i];
+
+			// skip if the current note is a chip
+			if (!note->hold)
+				continue;
+
+            // todo: the tick count for the ending double fx holds of speedstar is 1 extra (7 instead of 6)
+            // cant tell if vvd is ticking faster or if sdvx has a bug/"feature" that ending holds skip their second last tick
+            // looking at videos of it it definitley looks like its just skipping the second last tick
+            // all the information for ticks line up with these fx having 7 ticks, and the tick count for every other hold appears to be correct
+
+            // the first and last ticks of holds do not produce judgements
+            if (subbeat <= note->start_subbeat)
+                continue;
+            else if (subbeat >= note->end_subbeat)
+                continue;
+
+            // critical if the hold is being held
+            if (holds_held[i])
+                judgements[i] = JudgementCritical;
+            // error if the hold is not being held
+            else
+                judgements[i] = JudgementError;
+        }
+    }
+}
+
+void scoring_tick_changed(Scoring *scoring,
+                          int tick,
+                          int subbeat,
+                          Judgement bt_hold_judgements[CHART_BT_LANES],
+                          Judgement fx_hold_judgements[CHART_FX_LANES])
+{
+    // tick bt holds
+    tick_holds(CHART_BT_LANES,
+               scoring->current_bt_notes,
+               scoring->bt_holds_held,
+               tick,
+               subbeat,
+               bt_hold_judgements);
+
+    // tick fx holds
+    tick_holds(CHART_FX_LANES,
+               scoring->current_fx_notes,
+               scoring->fx_holds_held,
+               tick,
+               subbeat,
+               fx_hold_judgements);
 }
